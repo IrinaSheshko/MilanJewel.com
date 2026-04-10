46072d73d489275bc083627b1a1dd20c6f1dd04f
Irina Sheshko <irinasheshko1508@gmail.com>
2026-04-10 16:27:40 +0300

    fix: repair countdown date format and layout

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ search.addEventListener("keyup", function(event) {
 })
 
 function saleCountdown() {
-    const dateSale = new Date("Deсember 26, 2026 00:00")
+    const dateSale = new Date("December 26, 2026 00:00")
     const newDate = new Date();
     const daysNow = dateSale - newDate;
 
